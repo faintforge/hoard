ee6d1fd4e841203090f5e38abce2f6a52891f24d
Linus Kåreblom <linus.kareblom@gmail.com>
2026-01-26 16:41:19 +0100

    feat(core): General purpose hash map.

--- core.h
+++ core.h
@@ -92,6 +92,8 @@ extern void _log_log(log_level_t level, const char* file, int32_t line, const ch
         debug_break(); \
     }
 
+extern uint32_t fvn1a_hash32(uint32_t seed, const void* data, uint32_t size);
+
 // =============================================================================
 // ARENA ALLOCATOR
 // =============================================================================
@@ -112,6 +114,8 @@ extern allocator_t arena_allocator(arena_t* arena);
 
 extern void* arena_push(arena_t* arena, size_t size);
 extern void* arena_push_aligned(arena_t* arena, size_t size, size_t align);
+extern void* arena_push_zero(arena_t* arena, size_t size);
+extern void* arena_push_zero_aligned(arena_t* arena, size_t size, size_t align);
 extern void arena_reset(arena_t* arena);
 
 typedef struct arena_scope_t arena_scope_t;
@@ -149,6 +153,54 @@ extern void dyn_arr_pop(void** dyn_arr, void* output);
 
 extern void dyn_arr_push_arr(void** dyn_arr, const void* arr, size_t arr_length);
 extern void dyn_arr_pop_arr(void** dyn_arr, size_t count, void* output);
+
+// =============================================================================
+// HASH MAP
+// =============================================================================
+
+typedef struct hash_map_t hash_map_t;
+struct hash_map_t {
+    allocator_t allocator;
+
+    uint32_t key_size;
+    uint32_t value_size;
+
+    // AoS
+    void* key_array;
+    void* value_array;
+    uint32_t* hash_array;
+    uint8_t* state_array;
+
+    bool (*equal_func)(const void* lhs, const void* rhs, uint32_t size);
+    uint32_t (*hash_func)(const void* key, uint32_t size);
+
+    uint32_t capacity;
+    uint32_t count;
+    uint8_t load_factor;
+    float grow_factor;
+};
+
+typedef struct hash_map_desc_t hash_map_desc_t;
+struct hash_map_desc_t {
+    allocator_t allocator;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint32_t initial_capacity;
+    uint8_t load_factor;
+    float grow_factor;
+    bool (*equal_func)(const void* lhs, const void* rhs, uint32_t size);
+    uint32_t (*hash_func)(const void* key, uint32_t size);
+};
+
+extern hash_map_t hash_map_create(hash_map_desc_t desc);
+extern void hash_map_destroy(hash_map_t* map);
+
+extern bool hash_map_insert(hash_map_t* map, const void* key, const void* value);
+extern bool hash_map_set(hash_map_t* map, const void* key, const void* value, void* old_value);
+extern bool hash_map_remove(hash_map_t* map, const void* key, void* result_value);
+extern bool hash_map_contains(const hash_map_t* map, const void* key);
+extern bool hash_map_get(const hash_map_t* map, const void* key, void* result_value);
+extern void* hash_map_get_ptr(const hash_map_t* map, const void* key);
 
 #ifdef CORE_IMPLEMENTATION
 
@@ -192,6 +244,20 @@ void _log_log(log_level_t level, const char* file, int32_t line, const char* mes
         callback.func(event, callback.userdata);
         va_end(event.args);
     }
+}
+
+// =============================================================================
+// UTILITY
+// =============================================================================
+
+uint32_t fvn1a_hash32(uint32_t seed, const void* data, uint32_t size) {
+    // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+    uint32_t hash = 0x811c9dc5 ^ seed;
+    for (size_t i = 0; i < size; i++) {
+        hash ^= ((uint8_t*) data)[i];
+        hash *= 0x01000193;
+    }
+    return hash;
 }
 
 // =============================================================================
@@ -279,6 +345,7 @@ void* arena_push_aligned(arena_t* arena, size_t size, size_t align) {
     uintptr_t aligned_ptr = _align_up(current_ptr, align);
     uintptr_t position = aligned_ptr - (uintptr_t) arena->memory;
     if (position + size > arena->capacity) {
+        log_warn("Arena OOM");
         return NULL;
     }
     arena->last_position = position;
@@ -288,6 +355,16 @@ void* arena_push_aligned(arena_t* arena, size_t size, size_t align) {
 
 void* arena_push(arena_t* arena, size_t size) {
     return arena_push_aligned(arena, size, sizeof(void*));
+}
+
+void* arena_push_zero(arena_t* arena, size_t size) {
+    return arena_push_zero_aligned(arena, size, sizeof(void*));
+}
+
+void* arena_push_zero_aligned(arena_t* arena, size_t size, size_t align) {
+    void* mem = arena_push_aligned(arena, size, align);
+    memset(mem, 0, size);
+    return mem;
 }
 
 void arena_reset(arena_t* arena) {
@@ -492,6 +569,335 @@ void dyn_arr_pop_arr(void** dyn_arr, size_t count, void* output) {
     core_assert(count <= dyn_arr_length(*dyn_arr));
     size_t index = dyn_arr_length(*dyn_arr) - count;
     dyn_arr_remove_arr(dyn_arr, index, count, output);
+}
+
+// =============================================================================
+// HASH MAP
+// =============================================================================
+
+enum {
+    _HM_SLOT_EMPTY,
+    _HM_SLOT_ALIVE,
+    _HM_SLOT_DEAD,
+};
+
+static inline void* _hm_get_key_ptr(const hash_map_t* map, uint32_t index) {
+    return (uint8_t*) map->key_array + map->key_size*index;
+}
+
+static inline void* _hm_get_value_ptr(const hash_map_t* map, uint32_t index) {
+    return (uint8_t*) map->value_array + map->value_size*index;
+}
+
+// Call when inserting a new pair into the map.
+static bool _hm_resize_if_needed(hash_map_t* map) {
+    if (map->count < (uint32_t) (map->capacity * map->load_factor / 100.0f)) {
+        return false;
+    }
+
+    allocator_t alloc = map->allocator;
+
+    uint32_t new_capacity = map->capacity * map->grow_factor;
+    uint32_t new_count = 0;
+    void* new_key_array = core_alloc(alloc, map->key_size * new_capacity);
+    void* new_value_array = core_alloc(alloc, map->value_size * new_capacity);;
+    uint32_t* new_hash_array = core_alloc(alloc, sizeof(uint32_t) * new_capacity);;
+    uint8_t* new_state_array = core_alloc(alloc, sizeof(uint8_t) * new_capacity);;
+
+    memset(new_key_array, 0, map->key_size * new_capacity);
+    memset(new_value_array, 0, map->value_size * new_capacity);
+    memset(new_hash_array, 0, sizeof(uint32_t) * new_capacity);
+    memset(new_state_array, _HM_SLOT_EMPTY, sizeof(uint8_t) * new_capacity);
+
+    for (uint32_t i = 0; i < map->capacity; i++) {
+        if (map->state_array[i] != _HM_SLOT_ALIVE) {
+            continue;
+        }
+
+        uint32_t hash = map->hash_array[i];
+        uint32_t new_index = hash % new_capacity;
+
+        // Insert into new SoA
+        for (uint32_t j = 0; j < new_capacity; j++) {
+            if (new_state_array[new_index] == _HM_SLOT_EMPTY) {
+                break;
+            }
+
+            // Don't check for duplicate keys since those *shouldn't* exist.
+
+            new_index = (new_index + 1) % new_capacity;
+        }
+
+        core_assert(new_state_array[new_index] == _HM_SLOT_EMPTY);
+
+        void* key_ptr = (uint8_t*) new_key_array + map->key_size * new_index;
+        void* value_ptr = (uint8_t*) new_value_array + map->value_size * new_index;
+        memcpy(key_ptr, _hm_get_key_ptr(map, i), map->key_size);
+        memcpy(value_ptr, _hm_get_value_ptr(map, i), map->value_size);
+        new_hash_array[new_index] = hash;
+        new_state_array[new_index] = _HM_SLOT_ALIVE;
+        new_count++;
+    }
+
+    core_free(alloc, map->key_array, map->key_size * map->capacity);
+    core_free(alloc, map->value_array, map->value_size * map->capacity);
+    core_free(alloc, map->hash_array, sizeof(uint32_t) * map->capacity);
+    core_free(alloc, map->state_array, sizeof(uint8_t) * map->capacity);
+
+    map->capacity = new_capacity;
+    map->key_array = new_key_array;
+    map->value_array = new_value_array;
+    map->hash_array = new_hash_array;
+    map->state_array = new_state_array;
+    map->count = new_count;
+
+    return true;
+}
+
+hash_map_t hash_map_create(hash_map_desc_t desc) {
+    // Set sensilbe defaults
+    if (desc.initial_capacity == 0) {
+        desc.initial_capacity = 8;
+    }
+    if (desc.load_factor == 0) {
+        desc.load_factor = 80;
+    }
+    if (desc.grow_factor == 0.0f) {
+        desc.load_factor = 2.0f;
+    }
+
+    core_assert(desc.allocator.alloc != NULL);
+    core_assert(desc.key_size != 0);
+    core_assert(desc.value_size != 0);
+    core_assert(desc.equal_func != NULL);
+    core_assert(desc.hash_func != NULL);
+    core_assert((uint32_t) (desc.initial_capacity * desc.load_factor / 100.0f) > 0);
+    core_assert((uint32_t) (desc.initial_capacity * desc.grow_factor) > desc.initial_capacity);
+
+    allocator_t alloc = desc.allocator;
+    hash_map_t map = {
+        .allocator = alloc,
+        .key_size = desc.key_size,
+        .value_size = desc.value_size,
+        .key_array = core_alloc(alloc, desc.key_size * desc.initial_capacity),
+        .value_array = core_alloc(alloc, desc.value_size * desc.initial_capacity),
+        .hash_array = core_alloc(alloc, sizeof(uint32_t) * desc.initial_capacity),
+        .state_array = core_alloc(alloc, sizeof(uint8_t) * desc.initial_capacity),
+        .equal_func = desc.equal_func,
+        .hash_func = desc.hash_func,
+        .capacity = desc.initial_capacity,
+        .count = 0,
+        .load_factor = desc.load_factor,
+        .grow_factor = desc.grow_factor,
+    };
+
+    memset(map.key_array, 0, map.key_size * map.capacity);
+    memset(map.value_array, 0, map.value_size * map.capacity);
+    memset(map.hash_array, 0, sizeof(uint32_t) * map.capacity);
+    memset(map.state_array, _HM_SLOT_EMPTY, sizeof(uint8_t) * map.capacity);
+
+    return map;
+}
+
+void hash_map_destroy(hash_map_t* map) {
+    allocator_t alloc = map->allocator;
+    core_free(alloc, map->key_array, map->key_size * map->capacity);
+    core_free(alloc, map->value_array, map->value_size * map->capacity);
+    core_free(alloc, map->hash_array, sizeof(uint32_t) * map->capacity);
+    core_free(alloc, map->state_array, sizeof(uint8_t) * map->capacity);
+    *map = (hash_map_t) {0};
+}
+
+bool hash_map_insert(hash_map_t* map, const void* key, const void* value) {
+    uint32_t hash = map->hash_func(key, map->key_size);
+    uint32_t index = hash % map->capacity;
+    uint32_t i = 0;
+    while (true) {
+        uint8_t state = map->state_array[index];
+        if (state == _HM_SLOT_EMPTY) {
+            break;
+        }
+
+        // Key already exists
+        if (state == _HM_SLOT_ALIVE &&
+            map->hash_array[index] == hash &&
+            map->equal_func(key, _hm_get_key_ptr(map, index), map->key_size)) {
+            return false;
+        }
+
+        index = (index + 1) % map->capacity;
+
+        i++;
+        core_assert(i <= map->capacity);
+    }
+
+    core_assert(map->state_array[index] == _HM_SLOT_EMPTY);
+
+    memcpy(_hm_get_key_ptr(map, index), key, map->key_size);
+    memcpy(_hm_get_value_ptr(map, index), value, map->value_size);
+    map->hash_array[index] = hash;
+    map->state_array[index] = _HM_SLOT_ALIVE;
+    map->count++;
+
+    _hm_resize_if_needed(map);
+
+    return true;
+}
+
+bool hash_map_set(hash_map_t* map, const void* key, const void* value, void* old_value) {
+    uint32_t hash = map->hash_func(key, map->key_size);
+    uint32_t index = hash % map->capacity;
+    uint32_t i = 0;
+    while (true) {
+        uint8_t state = map->state_array[index];
+        if (state == _HM_SLOT_EMPTY) {
+            break;
+        }
+
+        // Key already exists
+        if (state == _HM_SLOT_ALIVE &&
+            map->hash_array[index] == hash &&
+            map->equal_func(key, _hm_get_key_ptr(map, index), map->key_size)) {
+            break;
+        }
+
+        index = (index + 1) % map->capacity;
+
+        i++;
+        core_assert(i <= map->capacity);
+    }
+
+    bool is_unique = map->state_array[index] == _HM_SLOT_EMPTY;
+    if (is_unique && old_value != NULL) {
+        memcpy(old_value, _hm_get_value_ptr(map, index), sizeof(map->value_size));
+    }
+
+    memcpy(_hm_get_value_ptr(map, index), value, map->value_size);
+    map->state_array[index] = _HM_SLOT_ALIVE;
+    if (is_unique) {
+        map->count++;
+    }
+
+    _hm_resize_if_needed(map);
+
+    return true;
+}
+
+bool hash_map_remove(hash_map_t* map, const void* key, void* result_value) {
+    uint32_t hash = map->hash_func(key, map->key_size);
+    uint32_t index = hash % map->capacity;
+    uint32_t i = 0;
+    while (true) {
+        uint8_t state = map->state_array[index];
+        if (state == _HM_SLOT_EMPTY) {
+            return false;
+        }
+
+        if (state == _HM_SLOT_ALIVE &&
+            map->hash_array[index] == hash &&
+            map->equal_func(key, _hm_get_key_ptr(map, index), map->key_size)) {
+            break;
+        }
+
+        index = (index + 1) % map->capacity;
+
+        i++;
+        core_assert(i <= map->capacity);
+    }
+
+    core_assert(map->state_array[index] == _HM_SLOT_ALIVE);
+
+    if (result_value != NULL) {
+        memcpy(result_value, _hm_get_value_ptr(map, index), sizeof(map->value_size));
+    }
+
+    map->state_array[index] = _HM_SLOT_DEAD;
+
+    return true;
+}
+
+bool hash_map_contains(const hash_map_t* map, const void* key) {
+    uint32_t hash = map->hash_func(key, map->key_size);
+    uint32_t index = hash % map->capacity;
+    uint32_t i = 0;
+    while (true) {
+        uint8_t state = map->state_array[index];
+        if (state == _HM_SLOT_EMPTY) {
+            return false;
+        }
+
+        // Key exists
+        if (state == _HM_SLOT_ALIVE &&
+            map->hash_array[index] == hash &&
+            map->equal_func(key, _hm_get_key_ptr(map, index), map->key_size)) {
+            return true;
+        }
+
+        index = (index + 1) % map->capacity;
+
+        i++;
+        core_assert(i <= map->capacity);
+    }
+    core_assert_msg(false, "Unreacable %s", __func__);
+    return -1;
+}
+
+bool hash_map_get(const hash_map_t* map, const void* key, void* result_value) {
+    uint32_t hash = map->hash_func(key, map->key_size);
+    uint32_t index = hash % map->capacity;
+    uint32_t i = 0;
+    while (true) {
+        uint8_t state = map->state_array[index];
+        if (state == _HM_SLOT_EMPTY) {
+            return false;
+        }
+
+        // Key exists
+        if (state == _HM_SLOT_ALIVE &&
+            map->hash_array[index] == hash &&
+            map->equal_func(key, _hm_get_key_ptr(map, index), map->key_size)) {
+            break;
+        }
+
+        index = (index + 1) % map->capacity;
+
+        i++;
+        core_assert(i <= map->capacity);
+    }
+
+    core_assert(map->state_array[index] == _HM_SLOT_ALIVE);
+
+    if (result_value != NULL) {
+        memcpy(result_value, _hm_get_value_ptr(map, index), sizeof(map->value_size));
+    }
+
+    return true;
+}
+
+void* hash_map_get_ptr(const hash_map_t* map, const void* key) {
+    uint32_t hash = map->hash_func(key, map->key_size);
+    uint32_t index = hash % map->capacity;
+    uint32_t i = 0;
+    while (true) {
+        uint8_t state = map->state_array[index];
+        if (state == _HM_SLOT_EMPTY) {
+            return NULL;
+        }
+
+        // Key exists
+        if (state == _HM_SLOT_ALIVE &&
+            map->hash_array[index] == hash &&
+            map->equal_func(key, _hm_get_key_ptr(map, index), map->key_size)) {
+            return _hm_get_value_ptr(map, index);
+        }
+
+        index = (index + 1) % map->capacity;
+
+        i++;
+        core_assert(i <= map->capacity);
+    }
+    core_assert_msg(false, "Unreacable %s", __func__);
+    return NULL;
 }
 
 #endif // CORE_IMPLEMENTATION
